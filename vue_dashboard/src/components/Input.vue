c7e8ff0fa4d9c323fbc2fcf4d112adefae2079de
ross-robertson-Joulea <ross.robertson@joulea.com>
2022-02-25 10:26:55 -0500

    Adds BEM and supporting code. Adds backend folder with a functioning flask sever that can be accessed from the vue app

--- vue_dashboard/src/components/Input.vue
+++ vue_dashboard/src/components/Input.vue
@@ -7,7 +7,9 @@
       <v-col
         md="5">
         <v-card>
+          <p>{{ msg }}</p>
           <v-card-title>Input Files</v-card-title>
+          <v-btn>{{ msg }}</v-btn>
           <v-card-text>Upload Weather File</v-card-text>
           <v-file-input
             accept="xls/*"
@@ -259,6 +261,278 @@
                           dense
                         ></v-select>
                       </v-row>
+                      <v-row>
+                        <v-select
+                          :items="Vent_Fan"
+                          label="Ventilation Fan"
+                          outlined
+                          dense
+                        ></v-select>
+                      </v-row>
+                      <v-row>
+                        <v-text-field
+                          label="Extra Ventilation Above Fresh Air Supply [liter/s]"
+                          outlined
+                          dense
+                          type="number"
+                        ></v-text-field>
+                      </v-row>
+                      <v-row>
+                        <v-select
+                          :items="Heat_Recov"
+                          label="Heat Recovery Type"
+                          outlined
+                          dense
+                        ></v-select>
+                      </v-row>
+                      <v-row>
+                        <v-select
+                          :items="Exhaust"
+                          label="Exhaust Air Recirculation %"
+                          outlined
+                          dense
+                        ></v-select>
+                      </v-row>
+                      <v-row>
+                        <v-text-field
+                          label="Building Air Leakage Level [m3/h]"
+                          outlined
+                          dense
+                          type="number"
+                        ></v-text-field>
+                      </v-row>
+                      <v-row>
+                        <v-text-field
+                          label="Specific Fan Power [W/(l/s)]"
+                          outlined
+                          dense
+                          type="number"
+                        ></v-text-field>
+                      </v-row>
+                      <v-row>
+                        <v-text-field
+                          label="Fan Flow Control Factor"
+                          outlined
+                          dense
+                          type="number"
+                        ></v-text-field>
+                      </v-row>
+                      <v-row>
+                        <v-select
+                          :items="Pump_C"
+                          label="Pump Control for Cooling"
+                          outlined
+                          dense
+                        ></v-select>
+                      </v-row>
+                      <v-row>
+                        <v-select
+                          :items="Pump_H"
+                          label="Pump Control for Heating"
+                          outlined
+                          dense
+                        ></v-select>
+                      </v-row>
+                    </v-expansion-panel-content>
+                  </v-expansion-panel>
+                  <v-expansion-panel>
+                    <v-expansion-panel-header>Domestic Hot Water (DHW)</v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                      <v-row>
+                        <v-select
+                          :items="DHW_Dis"
+                          label="DHW Distribution System"
+                          outlined
+                          dense
+                        ></v-select>
+                      </v-row>
+                      <v-row>
+                        <v-select
+                          :items="DHW_Gen"
+                          label="DHW Generation System"
+                          outlined
+                          dense
+                        ></v-select>
+                      </v-row>
+                    </v-expansion-panel-content>
+                  </v-expansion-panel>
+                  <v-expansion-panel>
+                    <v-expansion-panel-header>Building Energy Management Systems</v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                      <v-row>
+                        <v-select
+                          :items="BEM_Sys"
+                          label="Type of BEM System Installed [Source: prEN 15232:2006 5.3 and 8]"
+                          outlined
+                          dense
+                        ></v-select>
+                      </v-row>
+                    </v-expansion-panel-content>
+                  </v-expansion-panel>
+                  <v-expansion-panel>
+                    <v-expansion-panel-header>Photovoltaic System</v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                      <v-row>
+                        <v-text-field
+                          label="PV Module Surface Area (m2)"
+                          outlined
+                          dense
+                          type="number"
+                        ></v-text-field>
+                      </v-row>
+                      <v-row>
+                        <v-select
+                          :items="Orient"
+                          label="PV Module Orientation"
+                          outlined
+                          dense
+                        ></v-select>
+                      </v-row>
+                      <v-row>
+                        <v-select
+                          :items="Angle"
+                          label="PV Module Angle (degrees)"
+                          outlined
+                          dense
+                        ></v-select>
+                      </v-row>
+                      <v-row>
+                        <v-text-field
+                          label="PV Panel Peak Power Coeff (kW/m2)"
+                          outlined
+                          dense
+                          type="number"
+                        ></v-text-field>
+                      </v-row>
+                      <v-row>
+                        <v-text-field
+                          label="PV System Performance Factor"
+                          outlined
+                          dense
+                          type="number"
+                        ></v-text-field>
+                      </v-row>
+                    </v-expansion-panel-content>
+                  </v-expansion-panel>
+                  <v-expansion-panel>
+                    <v-expansion-panel-header>Solar Water Heating System</v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                      <v-row>
+                        <v-text-field
+                          label="Solar Collector Surface Area (m2)"
+                          outlined
+                          dense
+                          type="number"
+                        ></v-text-field>
+                      </v-row>
+                      <v-row>
+                        <v-select
+                          :items="Orient"
+                          label="SHW Module Orientation"
+                          outlined
+                          dense
+                        ></v-select>
+                      </v-row>
+                      <v-row>
+                        <v-select
+                          :items="Orient"
+                          label="SHW Module Angle (degrees)"
+                          outlined
+                          dense
+                        ></v-select>
+                      </v-row>
+                    </v-expansion-panel-content>
+                  </v-expansion-panel>
+                  <v-expansion-panel>
+                    <v-expansion-panel-header>Wind Turbine System</v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                      <v-row>
+                        <v-text-field
+                          label="Wind Turbine Diameter (m)"
+                          outlined
+                          dense
+                          type="number"
+                        ></v-text-field>
+                      </v-row>
+                      <v-row>
+                        <v-text-field
+                          label="Wind Turbine Efficiency"
+                          outlined
+                          dense
+                          type="number"
+                        ></v-text-field>
+                      </v-row>
+                    </v-expansion-panel-content>
+                  </v-expansion-panel>
+                  <v-expansion-panel>
+                    <v-expansion-panel-header>Energy Sources</v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                      <v-row>
+                        <v-select
+                          :items="E_Source"
+                          label="Primary Energy Source for Heating"
+                          outlined
+                          dense
+                        ></v-select>
+                      </v-row>
+                      <v-row>
+                        <v-select
+                          :items="E_Source"
+                          label="Primary Energy Source for DHW"
+                          outlined
+                          dense
+                        ></v-select>
+                      </v-row>
+                      <v-row>
+                        <v-select
+                          :items="E_Source"
+                          label="Primary Energy Source for Cooling"
+                          outlined
+                          dense
+                        ></v-select>
+                      </v-row>
+                      <v-row>
+                        <div class="text-center">
+                          <v-dialog
+                            v-model="dialog"
+                            width="500"
+                          >
+                            <template v-slot:activator="{ on, attrs }">
+                              <v-btn
+                                color="red lighten-2"
+                                dark
+                                v-bind="attrs"
+                                v-on="on"
+                              >
+                                Alter Primary and Emission Factors
+                              </v-btn>
+                            </template>
+
+                            <v-card>
+                              <v-card-title class="text-h5 grey lighten-2">
+                                Primary and Emission Factors
+                              </v-card-title>
+
+                              <v-card-text>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                              </v-card-text>
+
+                              <v-divider></v-divider>
+
+                              <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn
+                                  color="primary"
+                                  text
+                                  @click="dialog = false"
+                                >
+                                  Close
+                                </v-btn>
+                              </v-card-actions>
+                            </v-card>
+                          </v-dialog>
+                        </div>
+                      </v-row>
                     </v-expansion-panel-content>
                   </v-expansion-panel>
                 </v-expansion-panels>
@@ -297,10 +571,24 @@
 
 <script>
 
+import axios from 'axios'
+
 export default {
 
   name: "input",
   methods: {
+
+    getMessage() {
+      const path = 'http://127.0.0.1:5000/input'
+      axios.get(path)
+      .then((res) => {
+        this.msg = res.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+    },
+
     drawChart() {
       //Initialize the echarts instance based on the prepared dom
       let myChart = this.$echarts.init(document.getElementById("main"));
@@ -376,7 +664,12 @@ export default {
     this.drawChart();
   },
 
+  created() {
+    this.getMessage();
+  },
+
   data: () => ({
+
     terrain_class: ['Open Terrain', 'Country', 'Urban / City'],
     output_period: ['Hourly', 'Daily', 'Monthly'],
     panel: [],
@@ -419,7 +712,49 @@ export default {
             '36. Direct expansion single split system',
             '37. Direct expansion single split system including variable refrigerant flow systems'
           ],
-    lighting: ['Lighting Daylight Factor', 'Lighting Occupancy Factor', 'Lighting Constant Illumination Control Factor', 'Parasitic Lighting Energy']
+    Vent_Fan: ['1. Mechanical system only; no provision for natural ventilation',
+              '2. Mech. w/ optimal natural outside air cooling (whenever possible)',
+              '3. Same as 2, with fully natural fresh air supply (always)',
+              ],
+    Heat_Recov: ['No heat recovery',
+                'Heat exchange plates or pipes (0.65)',
+                'Two-elements-system (0.6)',
+                'Loading cold with air-conditioning (0.4)',
+                'Heat-pipes (0.6)',
+                'Slowly rotating or intermittent heat exchangers (0.7)',
+                ],
+    Exhaust: ['No exhaust air recirculation',
+              'Exhaust air recirculation 20%',
+              'Exhaust air recirculation  40%',
+              'Exhaust air recirculation  60%',
+              ],
+    Pump_C: ['No pump for cooling',
+            'Automatic control more than 50%',
+            'All other cases',
+            ],
+    Pump_H: ['No pump for heating',
+            'Automatic control more than 50%',
+            'All other cases',
+            ],
+    DHW_Dis: ['All Taps Within 3m from Heat Generation',
+              'Taps More Than 3m from Heat Generation',
+              'Circulation system or Unknown',
+              ],
+    DHW_Gen: ['Electric (0.75)',
+              'VR-Boiler (0.61)',
+              'Gas Boiler, HR-Boiler (0.75)',
+              'Co-Generation (0.9)',
+              'District Heating (0.9)',
+              'Heat Pump (1.4)',
+              'Steam (0.61)',
+              ],
+    BEM_Sys : ['Class D', 'Class C', 'Class B', 'Class A'],
+    Orient: ['S','SE','E','W','SW'],
+    Angle: ['0','30','45','60','90'],
+    E_Source: ['Electicity', 'Natural Gas', 'Fuel'],
+    lighting: ['Lighting Daylight Factor', 'Lighting Occupancy Factor', 'Lighting Constant Illumination Control Factor', 'Parasitic Lighting Energy'],
+    dialog: false,
+
   }),
 };
 </script>
